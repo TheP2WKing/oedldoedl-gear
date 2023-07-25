127f9fdaab702a12fca9029022d0dd8dba3282ad
TheP2WKing <tdziomba@hotmail.com>
2023-07-25 13:46:08 +0200

    added files

--- src/main/java/com/thep2wking/oedldoedl/gear/util/proxy/ServerProxy.java
+++
@@ -1,6 +0,0 @@
-package com.thep2wking.oedldoedl.gear.util.proxy;
-
-public class ServerProxy extends CommonProxy
-{
-
-}
---
+++ src/main/java/net/thep2wking/oedldoedlgear/util/proxy/ServerProxy.java
@@ -0,0 +1,4 @@
+package net.thep2wking.oedldoedlgear.util.proxy;
+
+public class ServerProxy {
+}
